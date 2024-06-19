d95c55cbb474561afd972ff0fd3b6fd3d99c2e3a
Lorenzo Mancini <l.mancini@cineca.it>
2024-06-19 18:14:44 +0200

    sviluppo tab programma

--- src/app/components/form-progetto/form-progetto.component.ts
+++ src/app/components/form-progetto/form-progetto.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import {  ItBackButtonComponent, ItButtonDirective, ItTabContainerComponent, ItTabItemComponent } from 'design-angular-kit';
-
+import { TabProgrammaComponent } from './components/tab-programma/tab-programma.component';
 @Component({
   selector: 'app-form-progetto',
   standalone: true,
   imports: [
     ItButtonDirective,
     ItTabContainerComponent,
-    ItTabItemComponent
+    ItTabItemComponent,
+    TabProgrammaComponent
   ],
   templateUrl: './form-progetto.component.html',
   styleUrl: './form-progetto.component.scss'
