891c0a62786361cb11c232e119aaa5319a970060
fventuri <f.venturi@cineca.it>
2024-06-24 12:29:39 +0200

    modale elimina media

--- src/app/components/dettaglio-progetto/dettaglio-progetto.component.ts
+++ src/app/components/dettaglio-progetto/dettaglio-progetto.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from '../../services/api.service';
 import { Media } from '../../shared/models/project.model';
 import { PaginatorPipe } from '../../shared/pipes/paginator.pipe';
 import { ItIconComponent, ItPaginationComponent, ItTooltipDirective } from 'design-angular-kit';
 import { DatePipe } from '@angular/common';
+import { Modal } from 'bootstrap';
 
 @Component({
   selector: 'app-dettaglio-progetto',
@@ -21,13 +22,16 @@ import { DatePipe } from '@angular/common';
 })
 export class DettaglioProgettoComponent {
   idProgetto:string = '';
-  mediaList: Media[] = [];
+  listaMedia: Media[] = [];
   changerValues: number[]=[5,10,15,20,25,30];
 
   centerCurrentPage = 0;
   pageElements = 5;
   pagesNumber = 1;
   changerValue: number = 5;
+  mediaToDelete: Media | null= null;
+  activeModal: any;
+  @ViewChild('eliminaModal') eliminaModal: any;
 
   constructor(
     private route: ActivatedRoute,
@@ -42,8 +46,8 @@ export class DettaglioProgettoComponent {
     console.log(id);
     this.apiService.getProgetti(this.idProgetto).subscribe(
       res=> {
-        res? this.mediaList = res.media : [];
-        this.pagesNumber = Math.round(this.mediaList.length / this.changerValue);
+        res? this.listaMedia = res.media : [];
+        this.pagesNumber = Math.round(this.listaMedia.length / this.changerValue);
       }
     )
 
@@ -58,12 +62,25 @@ export class DettaglioProgettoComponent {
   }
   changerEvent(value: number) {
     this.changerValue = value;
-    this.pagesNumber = Math.round(this.mediaList.length / this.changerValue);
+    this.pagesNumber = Math.round(this.listaMedia.length / this.changerValue);
     this.centerCurrentPage = 0;
   }
   anteprima(e:Event){
     e.preventDefault();
 
+  }
+
+  rimuoviFile(media : Media){
+    this.mediaToDelete = media;
+    this.activeModal=  new Modal(this.eliminaModal.nativeElement);
+    this.activeModal.show();
+  }
+
+  eliminaMedia(){
+    this.listaMedia = this.listaMedia.filter(b => b.id != this.mediaToDelete?.id);
+    this.activeModal.hide();
+    this.mediaToDelete = null;
+    this.activeModal = null;
   }
 
 }
